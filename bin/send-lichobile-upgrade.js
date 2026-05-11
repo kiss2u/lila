70b77b44295f3ff8e543363ce7d67c92cca903ee
Thibault Duplessis <t@lichess.org>
2026-05-11 08:41:47 +0200

    lichobile upgrade message sender

--- bin/send-lichobile-upgrade.js
+++ bin/send-lichobile-upgrade.js
@@ -1,51 +1,65 @@
-// import { MongoClient } from 'mongodb';
-//
-// const oauthToken = process.env.OAUTH_TOKEN;
-// const client = new MongoClient('mongodb://127.0.0.1:27917/lichess');
-// const lichessUrl = 'http://l.org';
-// const dryRun = true;
-//
-// // hardcode translations here
-// const translations = {
-//   en: `Upgrade to our new app: https://lichess.org/app`,
-// };
-//
-// const count = client.db().collection('lm_user_recent_nomobile').estimatedDocumentCount();
-//
-// console.log(`Sending messages to ${count} users...`);
-//
-// const chunkSize = 100;
-// const chunk = [];
+#!/usr/bin/env node
 
-// client.db().collection('lm_user_recent_nomobile').find().forEach(user => {
-//   // group by chunks of 100 users
-//   chunk.push(user._id);
-//   if (chunk.length >= chunkSize) processChunk();
-// });
-//
-// async function processChunk() {
-//
-//   const users = db.user4.find({ _id: { $in: chunk } },{$project:{lang:1}}).toArray();
-//
-//   if (dryRun) console.log(users.map(u => `${u._id} (${u.lang})`).join('\n'));
-//   else {
-//     const res = await fetch(`${lichessUrl}/inbox/${username}`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         Authorization: `Bearer ${oauthToken}`,
-//       },
-//       body: new URLSearchParams({ text }),
-//     });
-//
-//     if (!res.ok) console.error(`Failed to send message to ${username}: ${res.status} ${res.statusText}`);
-//   }
-//
-//   await new Promise(resolve => setTimeout(resolve, 500)); // Avoid hitting rate limits
-// });
-//
-// function makeMessage(langCode, url) {
-//   const lang = langCode.slice(0, 2);
-//   const translated = translations[lang] || translations.en;
-//   return translated.replace('{URL}', url);
-// }
+import { MongoClient } from 'mongodb';
+
+const oauthToken = process.env.OAUTH_TOKEN;
+// const client = new MongoClient('mongodb://127.0.0.1:27917/lichess');
+const client = new MongoClient('mongodb://127.0.0.1:27017/lichess');
+const lichessUrl = 'http://l.org';
+const dryRun = false;
+
+// hardcode translations here
+const translations = {
+  en: `Upgrade to our new app: https://lichess.org/app`,
+};
+
+const coll = client.db().collection('lm_user_recent_nomobile');
+
+const unsent = { sent: { $ne: true } };
+
+const count = await coll.countDocuments(unsent);
+console.log(`Sending messages to ${count} users...`);
+
+const langs = await coll.distinct('lang', unsent);
+
+for await (const lang of langs) {
+  console.log(`Processing language ${lang}...`);
+
+  const chunk = [];
+
+  for await (const user of coll.find({ lang, ...unsent }).limit(100)) {
+    chunk.push(user);
+    if (chunk.length >= 200) {
+      await processUsers(chunk, lang);
+      chunk.length = 0; // Clear the chunk
+    }
+  }
+  await processUsers(chunk, lang);
+}
+
+async function processUsers(users, lang) {
+  if (users.length === 0) return;
+  const ids = users.map(u => u._id);
+  const message = makeMessage(lang);
+  const command = `msg multi lichess ${ids.join(',')},thibault ${message}`;
+  if (dryRun) console.log(command);
+  else {
+    const res = await fetch(`${lichessUrl}/run/cli`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${oauthToken}`,
+      },
+      body: command,
+    });
+
+    if (!res.ok) console.error(`Failed to send message to ${username}: ${res.status} ${res.statusText}`);
+    else await coll.updateMany({ _id: { $in: ids } }, { $set: { sent: true } });
+  }
+
+  await new Promise(resolve => setTimeout(resolve, 500)); // Avoid hitting rate limits
+}
+
+function makeMessage(langCode) {
+  const lang = langCode.slice(0, 2);
+  return translations[lang] || translations.en;
+}
